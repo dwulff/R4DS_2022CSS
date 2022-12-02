38c7d8695af6ee113e9b81531c051681eefcf141
Dirk Wulff <dwulff@MacBook-Pro-von-xadmin.local>
2022-12-02 15:34:40 +0100

    ..

---
+++ TheRBootcamp/2_Code/Analyse_Interactive.R
@@ -0,0 +1,28 @@
+### R Skript zu "Analyse"
+### Kurs "R4DS @ CSS"
+### Datum: Dezember 2022
+
+
+### Einfache Statistiken ----------------------------
+
+# Lese die Datei Tourismus.csv als `daten` ein.
+
+# Berechne den Mittelwert (mean()) der Variablen `Besucher` und `Dauer`.
+
+# Berechne den Median (median()) der Variablen `Besucher` und `Dauer`.
+
+# Berechne die Standardabweichung (sd()) der Variablen `Besucher` und `Dauer`.
+
+# Berechne die Korrelation (cor()) zwischen den Variablen `Besucher` und `Dauer`.
+
+### Einfache Graphiken ----------------------------
+
+# Plotte ein Histogram (hist()) für die Variable `Besucher`.
+
+# Plotte ein Histogram (hist()) für die Variable `Dauer`.
+
+# Plotte ein Streudiagram (plot()) für die Variablen `Dauer` und `Besucher`.
+
+# Ändere die Achsen des Streudiagrams zu log-Skalen.
+
+# Plotte ein Boxplot für `Besucher` als Funktion von `Region`.
